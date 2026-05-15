5765c86f6c5c415cafb1b7b749d789d1c5aea0fc
Adikane-Dhruv <146655133+Adikane-Dhruv@users.noreply.github.com>
2026-05-15 17:03:30 +0000

    Intro

---
+++ 01vitereact/src/Adp.jsx
@@ -0,0 +1,7 @@
+function App(){
+    return(
+        <h3>React is ready to learn</h3>
+    )
+}
+ 
+export default App
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,26 @@
+// function App() {
+
+
+//   return (
+//     <h1>
+//       React with Vite | Adikane Dhruv
+//     </h1>
+//   )
+// }
+
+import Adp from "./Adp"
+
+function App() {
+
+
+  return (
+  <>
+    <Adp/>
+    <h1>Another para</h1>
+    <p>Thiss</p>
+  </>
+   
+  )
+}
+
+export default App
